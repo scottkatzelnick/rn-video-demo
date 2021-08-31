9d77f6b0d5e64e86b840a97ed224d83341df74de
Scott Katzelnick <scott@brailleskateboarding.com>
2021-08-31 02:15:30 -0400

    cleaned up VideoPlayer file

--- VideoPlayer.js
+++ VideoPlayer.js
@@ -6,8 +6,8 @@ import Video from 'react-native-video';
 const VideoPlayer = () => {
   //  The video we will play on the player.
   const uri =
-    // 'https://player.vimeo.com/external/591110988.hd.mp4?s=98538aca39e9bf77d73f2e5b488f4813637d013c&profile_id=174&oauth2_token_id=1528619126';
     'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4';
+
   const videoPlayer = useRef(null);
   const [duration, setDuration] = useState(0);
   const [paused, setPaused] = useState(true);
@@ -58,41 +58,26 @@ const VideoPlayer = () => {
     setCurrentTime(duration);
   };
 
-  const onFullScreen = () => {
-    setIsFullscreen(!isFullscreen);
-    // console.log(videoPlayer.current);
-    if (isFullscreen) {
-      console.log('isfull');
-      videoPlayer.current._onFullscreenPlayerWillPresent(true);
-      StatusBar.setHidden(true);
-    } else {
-      console.log('notfull');
-      videoPlayer.current._onFullscreenPlayerWillPresent(false);
-      StatusBar.setHidden(false);
-    }
-  };
-
   const videoDims = {
     height: (Dimensions.get('window').width * 9) / 16,
-    width: isFullscreen ? Dimensions.get('window').width : '100%',
+    width: Dimensions.get('window').width,
   };
 
   return (
     <View>
       <Video
-        onEnd={onEnd}
-        onLoad={onLoad}
-        onLoadStart={onLoadStart}
-        onProgress={onProgress}
-        paused={paused}
         ref={ref => (videoPlayer.current = ref)}
         resizeMode={'cover'}
         source={{uri}}
         style={videoDims}
+        paused={paused}
+        onLoadStart={onLoadStart}
+        onLoad={onLoad}
+        onProgress={onProgress}
+        onEnd={onEnd}
       />
       <MediaControls
-        isFullScreen={true}
-        onFullScreen={onFullScreen}
+        isFullScreen={false}
         duration={duration}
         isLoading={isLoading}
         progress={currentTime}
